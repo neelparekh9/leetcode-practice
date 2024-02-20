76264e9cf12754c23708fd844a7ccb939e504a06
neelparekh9 <parekh104@outlook.com>
2024-02-19 23:58:52 -0500

    YAAAAS

---
+++ Reverse list/sol.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        vector<int> arr;
+        ListNode *temp = head;
+
+        while (temp != NULL)
+        {
+            arr.push_back(temp->val);
+            // cout<<temp->val<<endl;
+            temp = temp->next;
+        }
+        ListNode *temp2 = head;
+        while (temp2 != NULL)
+        {
+            temp2->val = arr.back();
+            arr.pop_back();
+            temp2 = temp2->next;
+        }
+        return head;
+    }
+};
