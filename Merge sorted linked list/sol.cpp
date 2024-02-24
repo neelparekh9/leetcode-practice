9080eae0a4b7bb8e4aa2b03c7aca57a80b168b48
neelparekh9 <parekh104@outlook.com>
2024-02-24 15:29:34 -0500

    The streak is back baby

--- Merge sorted linked list/sol.cpp
+++ Merge sorted linked list/sol.cpp
@@ -15,15 +15,39 @@ class Solution
 public:
     ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
     {
-        ListNode *list3 = NULL;
-        ListNode *temp;
-
+        ListNode *list3 = new ListNode();
+        ListNode *temp = list3;
+        if (list1 == NULL)
+        {
+            return list2;
+        }
+        if (list2 == NULL)
+        {
+            return list1;
+        }
         while (list1 != NULL && list2 != NULL)
         {
-            if (list1->val == list2->val)
+            if (list1->val > list2->val)
             {
-                temp->val = list1->val;
+                temp->next = list2;
+                list2 = list2->next;
             }
+            else
+            {
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            temp = temp->next;
         }
+        if (list1)
+        {
+            temp->next = list1;
+        }
+        if (list2)
+        {
+            temp->next = list2;
+        }
+
+        return list3->next;
     }
 };
